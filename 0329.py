ac7da48d6117fd3978053ff3780fa1701691637c
xxivong <xxivong@naver.com>
2023-03-29 14:33:26 +0900

    수정해요

--- 0329.py
+++ 0329.py
@@ -7,3 +7,15 @@ var = "안녕"
 print(var)
 print(type(var))
 print(type(3))
+
+var1 = 3
+_var = 10
+height = 180
+weight = 70
+length_list = 120 # 변수명은 명확하게 나타낼 수 있도록 하자.
+
+a = 3
+b = 10 
+c = a + b
+a = c
+a = a + b
