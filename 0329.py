58581ff14255893f4ac11198f61704b563c56240
xxivong <xxivong@naver.com>
2023-03-29 15:02:29 +0900

    파이써언

--- 0329.py
+++ 0329.py
@@ -1,4 +1,5 @@
 # 0329
+'''
 print("안녕하세요.")
 print(3)
 print(10.6)
@@ -23,3 +24,15 @@ b = 10
 a *= b
 a = 3 * 10
 print("a : ", a)
+'''
+#name = input("이름이 뭔가요?")
+#print("제 이름은", name, "입니다.") 
+age = input("나이는?")
+print("나이는", age, "입니다.") #"20" , int("20")
+print("아버지 나이는", int(age)+30, "입니다.")
+#print(type(age))
+
+#int("30")
+#float("20.6")
+#str(30)
+
